5dbcbcd89d17a36c52fe8ee67eeb0e7cb129b5aa
Fereydoon Karimi <freydoonk@gmail.com>
2022-08-15 16:59:49 +0100

    Add picsum image

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <h1>My React App</h1>
       <h2>Welcome to My First React App</h2>
       <h3>The current time is: <Clock format={'HH:mm:ss'} ticking={true} timezone={'UK, GBR'} /></h3>
+      <img src='https://picsum.photos/500' alt='random'></img>
     </div>
   );
 }
