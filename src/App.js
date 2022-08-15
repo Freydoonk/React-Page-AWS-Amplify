83200f99557fd58989895ce69bb8ff89c5ebfcb1
Fereydoon Karimi <freydoonk@gmail.com>
2022-08-15 15:14:54 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Clock from 'react-live-clock';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My React App</h1>
+      <h2>Welcome to My First React App</h2>
+      <h3>The current time is: <Clock format={'HH:mm:ss'} ticking={true} timezone={'UK, GBR'} /></h3>
+    </div>
+  );
+}
+
+export default App;
